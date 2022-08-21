50de35bf44001dbf0542058f83e375bbd0d67153
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2022-08-21 11:25:57 +0300

    init

---
+++ data.js
@@ -0,0 +1,338 @@
+let data = [
+    {
+        title: 'NEAR Protocol',
+        slug: 'near-protocol',
+        oneliner: 'The highly scalable, developer-friendly blockchain.',
+        logo: 'https://awesome-near.s3.amazonaws.com/near-protocol.jpg',
+        website: 'https://near.org/'
+    },
+    {
+        title: 'NEAR Wallet',
+        slug: 'near-wallet',
+        oneliner: "Web wallet for NEAR Protocol which stores keys in browser's localStorage.",
+        logo: 'https://awesome-near.s3.amazonaws.com/near-wallet.jpg',
+        website: 'https://wallet.near.org/'
+    },
+    {
+        title: 'NEAR Explorer',
+        slug: 'near-explorer',
+        oneliner: 'NEAR blockchain explorer.',
+        logo: 'https://awesome-near.s3.amazonaws.com/near-explorer.jpg',
+        website: 'https://explorer.near.org'
+    },
+    {
+        title: 'Rainbow Bridge',
+        slug: 'rainbow-bridge',
+        oneliner: 'Transfer tokens between Ethereum and NEAR!',
+        logo: 'https://awesome-near.s3.amazonaws.com/rainbow-bridge.jpg',
+        website: 'https://rainbowbridge.app/'
+    },
+    {
+        title: 'Ref Finance',
+        slug: 'ref-finance',
+        oneliner: 'Multi-purpose DeFi platform built on NEAR Protocol.',
+        logo: 'https://awesome-near.s3.amazonaws.com/ref-finance.jpg',
+        website: 'https://ref.finance'
+    },
+    {
+        title: 'MetaMask',
+        slug: 'metamask',
+        oneliner: 'A crypto wallet & gateway to blockchain apps.',
+        logo: 'https://awesome-near.s3.amazonaws.com/metamask.jpg',
+        website: 'https://metamask.io/'
+    },
+    {
+        title: 'Trust Wallet',
+        slug: 'trust-wallet',
+        oneliner: 'Fast and secure Ethereum wallet that’s easy to use.',
+        logo: 'https://awesome-near.s3.amazonaws.com/trust-wallet.jpg',
+        website: 'https://trustwallet.com/'
+    },
+    {
+        title: 'Open Web Collective',
+        slug: 'open-web-collective',
+        oneliner: 'Build the Open Web, Unlock the Future.',
+        logo: 'https://awesome-near.s3.amazonaws.com/open-web-collective.jpg',
+        website: 'https://openwebcollective.com/'
+    },
+    {
+        title: 'Aurora',
+        slug: 'aurora-dev',
+        oneliner: 'Bridge + EVM Scaling Solution for Ethereum.',
+        logo: 'https://awesome-near.s3.amazonaws.com/aurora-dev.jpg',
+        website: 'https://aurora.dev'
+    },
+    {
+        title: 'HASH RUSH',
+        slug: 'hash-rush',
+        oneliner: 'Online sci-fi/fantasy RTS set in the fictional Hermeian galaxy.',
+        logo: 'https://awesome-near.s3.amazonaws.com/hash-rush.jpg',
+        website: 'https://hashrush.com/'
+    },
+    {
+        title: 'NEAR Docs',
+        slug: 'near-docs',
+        oneliner: 'Choose your path and start building.',
+        logo: 'https://awesome-near.s3.amazonaws.com/near-docs.jpg',
+        website: 'https://docs.near.org/'
+    },
+    {
+        title: 'D1 Ventures',
+        slug: 'd1-ventures',
+        oneliner: 'Powering the future of open economy.',
+        logo: 'https://awesome-near.s3.amazonaws.com/d1-ventures.jpg',
+        website: 'https://d1.ventures/'
+    },
+    {
+        title: 'Trisolaris',
+        slug: 'trisolaris',
+        oneliner: 'Bringing DeFi to the NEAR ecosystem.',
+        logo: 'https://awesome-near.s3.amazonaws.com/trisolaris.jpg',
+        website: 'https://www.trisolaris.io/'
+    },
+    {
+        title: 'The Graph',
+        slug: 'the-graph',
+        oneliner: 'Scalable Queries for a Decentralized Future.',
+        logo: 'https://awesome-near.s3.amazonaws.com/the-graph.jpg',
+        website: 'https://thegraph.com'
+    },
+    {
+        title: 'Octopus Network',
+        slug: 'octopus-network',
+        oneliner: 'A multi-chain network providing leased security and interoperability to Substrate appchains.',
+        logo: 'https://awesome-near.s3.amazonaws.com/octopus-network.jpg',
+        website: 'https://oct.network'
+    },
+    {
+        title: 'Paras',
+        slug: 'paras',
+        oneliner: 'Create, Trade, and Collect Digital Collectibles.',
+        logo: 'https://awesome-near.s3.amazonaws.com/paras.jpg',
+        website: 'https://paras.id'
+    },
+    {
+        title: 'NEARFans',
+        slug: 'nearfans',
+        oneliner: 'NEARFans is created by a group of NEAR builders.',
+        logo: 'https://awesome-near.s3.amazonaws.com/nearfans.jpg',
+        website: 'https://nearfans.com/'
+    },
+    {
+        title: 'Sender Wallet',
+        slug: 'senderwallet',
+        oneliner: 'Browser extension wallet built on NEAR.',
+        logo: 'https://awesome-near.s3.amazonaws.com/senderwallet.jpg',
+        website: 'https://senderwallet.io/'
+    },
+    {
+        title: 'MoonPay',
+        slug: 'moonpay',
+        oneliner: 'Growing the crypto economy.',
+        logo: 'https://awesome-near.s3.amazonaws.com/moonpay.jpg',
+        website: 'https://www.moonpay.io'
+    },
+    {
+        title: 'Burrow',
+        slug: 'burrow',
+        oneliner: 'Borrow, Supply. Thrive.',
+        logo: 'https://awesome-near.s3.amazonaws.com/burrow.jpg',
+        website: 'https://burrow.cash/'
+    },
+    {
+        title: 'NEAR University',
+        slug: 'near-education',
+        oneliner: 'Learn, earn, connect and certify in the NEAR community',
+        logo: 'https://awesome-near.s3.amazonaws.com/near-education.jpg',
+        website: 'https://www.near.university/'
+    },
+    {
+        title: 'IPFS',
+        slug: 'ipfs',
+        oneliner: 'A peer-to-peer hypermedia protocol to make the web faster, safer, and more open.',
+        logo: 'https://awesome-near.s3.amazonaws.com/ipfs.jpg',
+        website: 'https://ipfs.io'
+    },
+    {
+        title: 'Learn NEAR Club',
+        slug: 'learn-near',
+        oneliner: 'Learn how to use and build on NEAR and Earn NEAR.',
+        logo: 'https://awesome-near.s3.amazonaws.com/learn-near.jpg',
+        website: 'https://learnnear.club/'
+    },
+    {
+        title: 'Snark.Art',
+        slug: 'snark-art',
+        oneliner: 'Blockchain laboratory exploring creative & commercial possibilities in art.',
+        logo: 'https://awesome-near.s3.amazonaws.com/snark-art.jpg',
+        website: 'https://snark.art'
+    },
+    {
+        title: 'Machina',
+        slug: 'machina',
+        oneliner: 'Sharded data storage structure.',
+        logo: 'https://awesome-near.s3.amazonaws.com/machina.jpg',
+        website: 'https://machina.storage/'
+    },
+    {
+        title: 'Pad.Fi',
+        slug: 'nearpad',
+        oneliner: 'NearPad. Launchpad & DeFi hub for the NEAR ecosystem. ',
+        logo: 'https://awesome-near.s3.amazonaws.com/nearpad.jpg',
+        website: 'https://pad.fi/'
+    },
+    {
+        title: 'NEARWEEK',
+        slug: 'nearweek',
+        oneliner: 'Your Weekly NEAR Protocol Newsletter.',
+        logo: 'https://awesome-near.s3.amazonaws.com/nearweek.jpg',
+        website: 'https://nearweek.com/'
+    },
+    {
+        title: 'KYVE',
+        slug: 'kyve',
+        oneliner: 'Enabling limitless scalability for Web3.',
+        logo: 'https://awesome-near.s3.amazonaws.com/kyve.jpg',
+        website: 'https://www.kyve.network/'
+    },
+    {
+        title: 'GoNearName',
+        slug: 'gonearname',
+        oneliner: 'NEAR Account Marketplace - Sell, Bid and Buy NEAR Account Names.',
+        logo: 'https://awesome-near.s3.amazonaws.com/gonearname.jpg',
+        website: 'https://gonear.name/'
+    },
+    {
+        title: 'Ledger',
+        slug: 'ledger',
+        oneliner: 'Protect crypto assets with the most popular hardware wallet.',
+        logo: 'https://awesome-near.s3.amazonaws.com/ledger.jpg',
+        website: 'https://www.ledgerwallet.com/'
+    },
+    {
+        title: 'Meta Pool',
+        slug: 'meta-pool',
+        oneliner: 'Liquid Staking Protocol built on NEAR.',
+        logo: 'https://awesome-near.s3.amazonaws.com/meta-pool.jpg',
+        website: 'https://metapool.app/'
+    },
+    {
+        title: 'NEAR Tip Bot',
+        slug: 'near-tip-bot',
+        oneliner: 'Building the blockchain dApp inside the messenger.',
+        logo: 'https://awesome-near.s3.amazonaws.com/near-tip-bot.jpg',
+        website: 'https://tips.nearspace.info'
+    },
+    {
+        title: 'SputnikDAO',
+        slug: 'sputnikdao',
+        oneliner: 'Infra to create funding and squad DAOs',
+        logo: 'https://awesome-near.s3.amazonaws.com/sputnikdao.jpg',
+        website: 'https://sputnik.fund'
+    },
+    {
+        title: 'Berry Cards',
+        slug: 'berry-cards',
+        oneliner: 'Selecting berry hot boards from the berryclub.',
+        logo: 'https://awesome-near.s3.amazonaws.com/berry-cards.jpg',
+        website: 'https://berry.cards/'
+    },
+    {
+        title: '4NTS Guild',
+        slug: '4nts-guild',
+        oneliner: 'A community building and marketing focussed guild.',
+        logo: 'https://awesome-near.s3.amazonaws.com/4nts-guild.jpg',
+        website: 'https://nearguilds.com/'
+    },
+    {
+        title: 'Aurigami',
+        slug: 'aurigami',
+        oneliner: 'Lend, borrow, and earn with ease on Aurora. ',
+        logo: 'https://awesome-near.s3.amazonaws.com/aurigami.jpg',
+        website: 'https://www.aurigami.finance/'
+    },
+    {
+        title: 'Allbridge',
+        slug: 'allbridge',
+        oneliner: 'A fast, affordable and secure way of moving liquidity between EVM, non-EVM, and L2 blockchains.',
+        logo: 'https://awesome-near.s3.amazonaws.com/allbridge.jpg',
+        website: 'https://allbridge.io/'
+    },
+    {
+        title: 'Astro DAO',
+        slug: 'astrodao',
+        oneliner: 'Launch your DAO in less than 10 minutes without a line of code.',
+        logo: 'https://awesome-near.s3.amazonaws.com/astrodao.jpg',
+        website: 'https://astrodao.com/'
+    },
+    {
+        title: 'Mintbase',
+        slug: 'mintbase',
+        oneliner: 'Digital assets minted and backed by you.',
+        logo: 'https://awesome-near.s3.amazonaws.com/mintbase.jpg',
+        website: 'https://mintbase.io/'
+    },
+    {
+        title: 'Metamon',
+        slug: 'metamon',
+        oneliner: 'Free-to-play Play-to-Earn online Battle Royale Game and Sci-Fi Franchise.',
+        logo: 'https://awesome-near.s3.amazonaws.com/metamon.jpg',
+        website: 'https://www.metamon.app/'
+    },
+    {
+        title: 'MintGate',
+        slug: 'mintgate',
+        oneliner: 'Turn any online content into exclusive rewards.',
+        logo: 'https://awesome-near.s3.amazonaws.com/mintgate.jpg',
+        website: 'https://mintgate.app'
+    },
+    {
+        title: 'Math Wallet',
+        slug: 'math-wallet',
+        oneliner: "World's First Insured Crypto Wallet.",
+        logo: 'https://awesome-near.s3.amazonaws.com/math-wallet.jpg',
+        website: 'https://www.mathwallet.org'
+    },
+    {
+        title: 'NEAR Guilds',
+        slug: 'near-guilds',
+        oneliner: 'Open-source innovation for the Open Web and the NEAR Ecosystem.',
+        logo: 'https://awesome-near.s3.amazonaws.com/near-guilds.jpg',
+        website: 'https://nearguilds.com'
+    },
+    {
+        title: 'ZED',
+        slug: 'zed',
+        oneliner: 'Build an empire with your own champion racehorses.',
+        logo: 'https://awesome-near.s3.amazonaws.com/zed.jpg',
+        website: 'https://www.zed.run/'
+    },
+    {
+        title: 'MyNearWallet',
+        slug: 'mynearwallet',
+        oneliner: 'NEAR wallet to store, buy, send and stake assets for DeFi.',
+        logo: 'https://awesome-near.s3.amazonaws.com/mynearwallet.jpg',
+        website: 'https://mynearwallet.com/'
+    },
+    {
+        title: 'Decentral Bank',
+        slug: 'decentral-bank',
+        oneliner: 'DAO issuing NEAR-native stablecoins - USN.',
+        logo: 'https://awesome-near.s3.amazonaws.com/decentral-bank.jpg',
+        website: 'https://decentral-bank.finance/'
+    },
+    {
+        title: 'AwesomeNEAR',
+        slug: 'awesomenear',
+        oneliner: 'Curated list of all projects building on NEAR and Aurora.',
+        logo: 'https://awesome-near.s3.amazonaws.com/awesome-logo.jpg',
+        website: 'https://awesomenear.com'
+    },
+    {
+        title: 'Aurorascan',
+        slug: 'aurorascan',
+        oneliner: 'Aurora Explorer powered by Etherscan.',
+        logo: 'https://awesome-near.s3.amazonaws.com/aurorascan.jpg',
+        website: 'https://aurorascan.dev/'
+    }
+];
